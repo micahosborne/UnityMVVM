4c5d9f3a3b99f9b5fa7ad8fd77fc388f86f2d86e
Micah Osborne <micahosborne@gmail.com>
2014-08-01 13:14:25 -0400

    Update ICommand.cs

--- ICommand.cs
+++ ICommand.cs
@@ -10,10 +10,3 @@ namespace System.Windows.Input {
 		event EventHandler CanExecuteChanged;
 	}
 }
-using System.Runtime.CompilerServices;
-namespace System.Collections.Specialized {
-	public interface INotifyCollectionChanged
-	{
-		event NotifyCollectionChangedEventHandler CollectionChanged;
-	}
-}
